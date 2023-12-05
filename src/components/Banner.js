368992ce9194f63c4c77fae5ceb34c798a89e91e
Zongo Maqutu <MQTZON001@myuct.ac.za>
2023-12-05 21:15:48 +0200

    added structure to banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Banner() {
   return (
     <>
-        <p>Banner</p>
+    	<section className="banner">
+        	<div className="container grid">
+				<div className="banner-information">
+					<h1>Little Lemon</h1>
+					<h2>Cape Town</h2>
+					<p>
+						We are a family owned Mediterranean restaurant, 
+						focused on traditional recipes served with a modern twist.
+					</p>
+					<Link className="button-primary" to={pages.get('reservations').path}>
+						Reserve a table
+					</Link>
+				</div>
+				<img className="hero-image" src={restaurantFoodImage} alt="Restaurant food" />
+      		</div>
+    	</section>
     </>
   )
 }
