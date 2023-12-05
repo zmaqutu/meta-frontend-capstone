63e913d0c5631924cf663e2bce737b71b28e1a7e
Zongo Maqutu <MQTZON001@myuct.ac.za>
2023-12-05 18:54:34 +0200

    added basic structure to the nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import Link from 'react-router-dom'
 
 function Header() {
   return (
     <header>
         <nav>
+          <Link>
+            This is where the logo goes
+          </Link>
+          <button>
+            This is where the menu goes
+          </button>
+          <Link>
+            This is where the link to the home page goes
+          </Link>
+          <Link>
+            This is where the link to the about page goes
+          </Link>
+          <Link>
+            This is where the link to the menu page goes
+          </Link>
+          <Link>
+            This is where the link to the reservations page goes
+          </Link>
+          <Link>
+          <Link>
+            This is where the link to the contact page goes
+          </Link>
+            This is where the link to the order online goes
+          </Link>
+
             
         </nav>
     </header>
