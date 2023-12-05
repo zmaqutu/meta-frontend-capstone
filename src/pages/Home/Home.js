aba510aea1eb72f65562d745a5fd241972ac0691
Zongo Maqutu <MQTZON001@myuct.ac.za>
2023-12-05 20:13:26 +0200

    added Banner, Testimonials and Specials to Home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from '../../components/Banner/Banner'
+import WeeklySpecials from '../../components/WeeklySpecials/WeeklySpecials'
+import Testimonials from '../../components/Testimonials/Testimonials'
+
+function Home() {
+  return (
+    <>
+      <Banner />
+      <WeeklySpecials />
+      <Testimonials />
+    </>
+  )
+}
+
+export default Home
