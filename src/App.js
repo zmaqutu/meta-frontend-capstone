dd877eff6f6fdd2a0c9d538e794c18921734f87b
Zongo Maqutu <MQTZON001@myuct.ac.za>
2023-12-05 20:07:35 +0200

    added trivial routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import pageConfigs from './utils/pageConfigs';
 
 function App() {
   return (
     <>
     <Header />
+    <main>
+      <Routes>
+        <Route path={pageConfigs.get('home').path} element={<Home/>} />
+        <Route path={pageConfigs.get('about').path} element={<About/>} />
+        <Route path={pageConfigs.get('menu').path} element={<Menu/>} />
+        <Route path={pageConfigs.get('reservations').path} element={<Bookings/>} />
+        <Route path={pageConfigs.get('confirmedReservation').path} element={<ConfirmedBooking/>} />
+        <Route path={pageConfigs.get('contact').path} element={<Contact/>} />
+      </Routes>
+    </main>
     </>
   );
 }
