cb24adc598a3185b20ee00dc30b21ded5f776bba
Zongo Maqutu <MQTZON001@myuct.ac.za>
2023-12-05 21:58:35 +0200

    completed styling and verified usability

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import { Route, Routes } from 'react-router-dom';
-import pageConfigs from './utils/pageConfigs';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Menu from './pages/Menu/Menu';
-import Reservations from './pages/Reservations/Reservations';
-import ConfirmedReservation from './pages/ConfirmedReservation/ConfirmedReservation';
-import Contact from './pages/Contact/Contact';
+import {
+  Route, 
+  Routes
+} from 'react-router-dom';
+import pages from './utils/pages';
+import Layout from './components/layout/Layout';
+import Home from './components/pages/Home';
+import Bookings from './components/pages/Bookings';
+import ConfirmedBooking from './components/pages/Bookings/ConfirmedBooking';
+import NotFound from './components/pages/NotFound';
+import UnderConstruction from './components/pages/UnderConstruction';
 
-function App() {
+const App = () => {
   return (
     <>
-    <Header />
-    <main>
-      <Routes>
-        <Route path={pageConfigs.get('home').path} element={<Home/>} />
-        <Route path={pageConfigs.get('about').path} element={<About/>} />
-        <Route path={pageConfigs.get('menu').path} element={<Menu/>} />
-        <Route path={pageConfigs.get('reservations').path} element={<Reservations/>} />
-        <Route path={pageConfigs.get('confirmedReservation').path} element={<ConfirmedReservation/>} />
-        <Route path={pageConfigs.get('contact').path} element={<Contact/>} />
-      </Routes>
-    </main>
+      <Layout>
+        <Routes>
+          <Route path={pages.get('home').path} element={<Home />} />
+          <Route 
+            path={pages.get('about').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('menu').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path={pages.get('bookings').path} element={<Bookings />} />
+          <Route 
+            path={pages.get('confirmedBooking').path} 
+            element={<ConfirmedBooking />} 
+          />
+          <Route 
+            path={pages.get('orderOnline').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('login').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </>
   );
-}
+};
 
 export default App;
